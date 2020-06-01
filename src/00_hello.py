172ab7f8f5d9346b9432ed2f6f9921bbf31852ea
Yonipineda <yonipineda1010@icloud.com>
2020-06-01 14:21:31 -0400

    First 2 problems done. will finish the rest later

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,7 @@
 # Print "Hello, world!" to your terminal
+
+def hello():
+    '''
+    Hello world, again.
+    '''
+    print('hello')
--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,13 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+# with a function. 
+# Unfamiliar with vscode. I've used Atom. This is better but still tricky to use
+
+def func(x, y):
+    return x**y 
+
+a = func(2, 65536)
+
+print(f'Big Num: {a}')
